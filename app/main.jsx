2331f2432655235b7a3e41f8efa085f14a83a89b
Borja Guzman <borja.guzman@lastminute.com>
2017-09-25 10:28:01 +0200

    Initial commit

---
+++ app/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function App () {
+  return (
+    <div>This is APP!</div>
+  );
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#demo'));
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const hotLoadEntryPoints = [
+  'react-hot-loader/patch',
+  'webpack-dev-server/client?http://localhost:9000',
+  'webpack/hot/only-dev-server'
+];
+
+module.exports = {
+  entry: {
+    main: ['./app/main.jsx', ...hotLoadEntryPoints],
+    vendor: ['react', 'react-dom', ...hotLoadEntryPoints]
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx$/,
+      use: [{ loader: 'babel-loader' }]
+    }]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'dist/vendor.bundle.js' }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'dist/[name].bundle.js'
+  },
+
+  devServer: {
+    contentBase: path.resolve('./dist'),
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+    historyApiFallback: true
+  },
+
+  stats: {
+    colors: true
+  }
+}
